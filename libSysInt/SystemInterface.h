74e35894310e7cca0ee6a89fee078df3fc5e7580
OSM-Made <oldskoolmodz@gmail.com>
2025-11-02 13:05:38 -0700

    Add default args for jailbreak

--- libSysInt/SystemInterface.h
+++ libSysInt/SystemInterface.h
@@ -1,6 +1,6 @@
 #pragma once
 
-bool Jailbreak(uint64_t authId, bool nullSandboxPath);
+bool Jailbreak(uint64_t authId = -1, bool nullSandboxPath = false);
 bool RestoreJail();
 int LoadSprx(int processId, std::string libraryPath);
 bool UnloadSprx(int processId, int libraryHandle);
